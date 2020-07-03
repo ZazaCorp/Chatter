940b119af8680cc50fd698920933b55eda9292b8
William Kwadwo Owusu <williamowusu1998@gmail.com>
2020-07-03 19:22:31 +0100

    Update messages.test.js

--- messages.test.js
+++ messages.test.js
@@ -1,9 +1,10 @@
 const formatMessage = require('./utils/messages');
 const moment = require('moment');
+const date = Date.UTC();
 const text = "Hello everyone"
 const username = "John";
 //"time" for test should be current time on testers clock
 //for example if the testers computer time is currently 6:28 pm the time that should be inputted in the test is "6:28 pm"
 test("return username, text and time message was sent", ()=>{
-    expect(formatMessage(username, text)) .toBe({"text": "Hello everyone", "time": moment(), "username": "John"})
+    expect(formatMessage(username, text)) .toBe({"text": "Hello everyone", "time": date, "username": "John"})
 });
