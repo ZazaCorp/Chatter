1f590f7e9832c0b1703e5c68949416a34f0c119f
William Kwadwo Owusu <williamowusu1998@gmail.com>
2020-07-03 19:32:12 +0100

    Update messages.test.js

--- messages.test.js
+++ messages.test.js
@@ -1,10 +1,10 @@
 const formatMessage = require('./utils/messages');
 const moment = require('moment');
-const date = Date.UTC();
+const now = moment();
 const text = "Hello everyone"
 const username = "John";
 //"time" for test should be current time on testers clock
 //for example if the testers computer time is currently 6:28 pm the time that should be inputted in the test is "6:28 pm"
 test("return username, text and time message was sent", ()=>{
-    expect(formatMessage(username, text)) .toStrictEqual({"text": "Hello everyone", "time": moment.format()", "username": "John"})
+    expect(formatMessage(username, text)) .toStrictEqual({"text": "Hello everyone", "time": now.format()", "username": "John"})
 }); 
