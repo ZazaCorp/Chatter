f71ad913fe5e2e7d72b043b9856ff6e95366473a
William Kwadwo Owusu <williamowusu1998@gmail.com>
2020-07-03 19:15:04 +0100

    Update messages.test.js

--- messages.test.js
+++ messages.test.js
@@ -5,5 +5,5 @@ const username = "John";
 //"time" for test should be current time on testers clock
 //for example if the testers computer time is currently 6:28 pm the time that should be inputted in the test is "6:28 pm"
 test("return username, text and time message was sent", ()=>{
-    expect(formatMessage(username, text)) .toStrictEqual({"text": "Hello everyone", "time": moment, "username": "John"})
+    expect(formatMessage(username, text)) .toBe({"text": "Hello everyone", "time": moment(), "username": "John"})
 });
